af17f07f8317989d58024e6ab6370ec76f3069ab
Nicholas <Nicholas.wall82@ed.cna.nl.ca>
2023-03-21 15:21:06 -0230

    couple lines of code to show this is working

--- main.py
+++ main.py
@@ -1,16 +1,14 @@
-# This is a sample Python script.
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
-
-
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+def displayCard(card):
+    print(card[0])
+    print(card[1])
+    print(card[2])
 
 
-# Press the green button in the gutter to run the script.
+def main():
+    print("First message for blackjack project")
+    card = ["Hearts", "Ace", 11]
+    displayCard(card)
+
 if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+    main()
