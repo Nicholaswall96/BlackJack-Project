f989d6cdfc3e2f40424404620d7f1d377adab106
Nicholas <Nicholas.wall82@ed.cna.nl.ca>
2023-04-12 18:47:47 -0230

    Added some code for reading/writing the money amount. Find out later if it works

--- db.py
+++ db.py
@@ -1,8 +1,21 @@
 FILENAME = "Money.txt"
 
-def writeMoney():
-    pass
-
+def writeMoney(payOut):
+    try:
+        with open (FILENAME, "w", newline = "") as file:
+            file.write(str(payOut))
+    except Exception as e:
+        print(type(e), e)
 
 def readMoney():
-    pass
+    try:
+        moneyAmount = []
+        with open(FILENAME, "r", newline = "") as file:
+            money = file.readline()
+            moneyAmount.append(money)
+            print("Money:\t" + moneyAmount[0][0])
+            return float(moneyAmount[0][0])
+    except FileNotFoundError:
+        print("Could not find the file. try again")
+    except Exception as e:
+        print(type(e), e)
